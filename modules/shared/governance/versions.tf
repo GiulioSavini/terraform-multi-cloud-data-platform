8f85e9dc736c3e491021d8553f7be560892478bc
giulio savini <giuliosavini@proton.me>
2026-03-19 21:13:29 +0100

    refactor: enhance shared and GCP modules with improved configs

--- modules/shared/governance/versions.tf
+++ modules/shared/governance/versions.tf
@@ -1,8 +1,18 @@
 terraform {
-  required_version = ">= 1.9.0"
+  required_version = ">= 1.7.0"
+
   required_providers {
-    aws     = { source = "hashicorp/aws", version = "~> 5.0" }
-    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
-    google  = { source = "hashicorp/google", version = "~> 5.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
   }
 }
